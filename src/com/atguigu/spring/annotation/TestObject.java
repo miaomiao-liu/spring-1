360954e3980e484a5dc204af67a7806300661ac8
miaomiao <1242019260@qq.com>
2018-01-19 17:14:38 +0800

    Spring_通过注解配置Bean

---
+++ src/com/atguigu/spring/annotation/TestObject.java
@@ -0,0 +1,10 @@
+package com.atguigu.spring.annotation;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by miaomiao on 18-1-19.
+ */
+@Component
+public class TestObject {
+}
---
+++ src/com/atguigu/spring/annotation/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.atguigu.spring.annotation.repository;
+
+/**
+ * Created by miaomiao on 18-1-19.
+ */
+public interface UserRepository {
+
+    void save();
+}
